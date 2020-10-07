212dbb077ee4f710b8e27dbe7396e41898c9988e
yjsong67 <yj_song67@naver.com>
2020-10-08 00:08:05 +0900

    mypage 페이지 연결

---
+++ app/src/main/java/com/example/capstone2_v1/FriendMain.java
@@ -0,0 +1,16 @@
+package com.example.capstone2_v1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FriendMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friend);
+
+
+    }
+}
--- app/src/main/java/com/example/capstone2_v1/menufragment/MypageMenu.java
+++ app/src/main/java/com/example/capstone2_v1/menufragment/MypageMenu.java
@@ -1,12 +1,15 @@
 package com.example.capstone2_v1.menufragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
+import com.example.capstone2_v1.FriendMain;
 import com.example.capstone2_v1.R;
 
 
@@ -23,8 +26,18 @@ public class MypageMenu extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mypage, container, false);
 
+        Button button = (Button) view.findViewById(R.id.buttonfriendList);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), FriendMain.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
+
 
 }
 
