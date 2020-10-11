dd568e2c003d36dbb0ab7ec7af8865973e64a6fc
applejuice8382 <lemon585@naver.com>
2020-10-11 23:34:38 +0900

    search

---
+++ app/src/main/java/com/example/capstone2_v1/SearchMenu.java
@@ -0,0 +1,85 @@
+package com.example.capstone2_v1;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.capstone2_v1.R;
+import com.example.capstone2_v1.searchfragment.Fragment1;
+import com.example.capstone2_v1.searchfragment.Fragment2;
+import com.example.capstone2_v1.searchfragment.PagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+
+
+public class SearchMenu extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("관광지"));
+        tabLayout.addTab(tabLayout.newTab().setText("사용자"));
+
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+
+        });
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item ){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
+
+
+
