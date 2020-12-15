d4f2633653737deaf9f4b99342bf64d68b1d9a2b
applejuice8382 <lemon585@naver.com>
2020-12-15 18:18:42 +0900

    로그인 remember

--- app/src/main/java/com/example/capstone2_v1/LoginActivity.java
+++ app/src/main/java/com/example/capstone2_v1/LoginActivity.java
@@ -1,16 +1,19 @@
 package com.example.capstone2_v1;
 
+import android.content.Context;
 import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.capstone2_v1.request.LoginRequest;
+import com.example.capstone2_v1.request.PreferenceManager;
 
 import org.json.JSONObject;
 
@@ -21,26 +24,30 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText idText, passwordText;
     Button loginButton, signupButton;
-
+    private CheckBox cb_save;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mContext = this;
 
         idText =(EditText)findViewById(R.id.idText);
         passwordText =(EditText)findViewById(R.id.pswordText);
 
         loginButton = (Button)findViewById(R.id.loginButton);
         signupButton = (Button)findViewById(R.id.signupButton);
+        cb_save = (CheckBox)findViewById(R.id.checkBox);
 
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
+        boolean boo = PreferenceManager.getBoolean(mContext,"check"); //로그인 정보 기억하기 체크 유무 확인
+         if(boo){ // 체크가 되어있다면 아래 코드를 수행 //저장된 아이디와 암호를 가져와 셋팅한다.
+              idText.setText(PreferenceManager.getString(mContext, "id"));
+              passwordText.setText(PreferenceManager.getString(mContext, "pw"));
+              cb_save.setChecked(true); //체크박스는 여전히 체크 표시 하도록 셋팅
+              }
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +86,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,6 +95,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(signupIntent);
             }
         });
+
+        //로그인 기억하기 체크박스 유무에 따른 동작 구현
+
+        cb_save.setOnClickListener(new CheckBox.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox)v).isChecked()) {
+                    // 체크박스 체크 되어 있으면
+                    // editText에서 아이디와 암호 가져와 PreferenceManager에 저장한다.
+                    PreferenceManager.setString(mContext, "id", idText.getText().toString()); //id 키값으로 저장
+                    PreferenceManager.setString(mContext, "pw", passwordText.getText().toString()); //pw 키값으로 저장
+                    PreferenceManager.setBoolean(mContext, "check", cb_save.isChecked()); //현재 체크박스 상태 값 저장
+                } else { //체크박스가 해제되어있으면
+                    PreferenceManager.setBoolean(mContext, "check", cb_save.isChecked()); //현재 체크박스 상태 값 저장
+                    PreferenceManager.clear(mContext); //로그인 정보를 모두 날림
+                }
+            }
+        }) ;
+
 
     }
 
