447dd89706835c600e16973bbdfd7a1173ac2e4b
applejuice8382 <lemon585@naver.com>
2020-12-18 14:01:39 +0900

    favorite 변경

--- app/src/main/java/com/example/capstone2_v1/request/FavoriteRequest.java
+++ app/src/main/java/com/example/capstone2_v1/request/FavoriteRequest.java
@@ -6,22 +6,22 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FavoriteRequest extends StringRequest {
-    final static private String URL = "http://192.168.35.21:8070/favorite.php";
-    private Map<String, String> parameters;
-
-    public FavoriteRequest(String tour_name, Response.Listener<String> listner) {
-        super(Method.POST, URL, listner, null);
-        try {
-            parameters = new HashMap<>();
-            parameters.put("tour_name", tour_name);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected Map<String, String> getParams() {
-        return parameters;
-    }
-}
+//public class FavoriteRequest extends StringRequest {
+//    final static private String URL = "http://192.168.35.21:8070/favorite.php";
+//    private Map<String, String> parameters;
+//
+//    public FavoriteRequest(String tour_name, Response.Listener<String> listner) {
+//        super(Method.POST, URL, listner, null);
+//        try {
+//            parameters = new HashMap<>();
+//            parameters.put("tour_name", tour_name);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    protected Map<String, String> getParams() {
+//        return parameters;
+//    }
+//}
